4e513efde3396faad57e3095d65af7f2f3a0d2e8
cyril:) <cyril.grout@epitech.eu>
2021-12-22 15:48:54 +0100

    add instant app conf

--- ios/reactnativeinstantboilerplateTests/reactNativeBoilerplateTests.m
+++ ios/reactnativeinstantboilerplateTests/reactNativeBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativeboilerplateTests : XCTestCase
+@interface reactnativeinstantboilerplateTests : XCTestCase
 
 @end
 
-@implementation reactnativeboilerplateTests
+@implementation reactnativeinstantboilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
