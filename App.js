d812a370e0686d96dda5982e4c62726856704e42
cyril:) <cyril.grout@epitech.eu>
2021-12-22 12:09:28 +0100

    Add blank app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  const body = 'Blank React Native App';
+
+  return (
+    <View>
+      <Text>{body}</Text>
+    </View>
+  );
+};
+
+export default App;
